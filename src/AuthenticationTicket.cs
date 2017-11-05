f61a270e955e23ab2fde91f5ce342ad5e02719c6
Dariel Marlow <dmarlow@gmail.com>
2017-11-05 12:40:17 -0800

    Initial commit

---
+++ src/AuthenticationTicket.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace BearerTokenBridge
+{
+    public class AuthenticationTicket
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthenticationTicket"/> class
+        /// </summary>
+        /// <param name="identity"></param>
+        /// <param name="properties"></param>
+        public AuthenticationTicket(ClaimsIdentity identity, AuthenticationProperties properties)
+        {
+            Identity = identity;
+            Properties = properties ?? new AuthenticationProperties();
+        }
+
+        /// <summary>
+        /// Gets the authenticated user identity.
+        /// </summary>
+        public ClaimsIdentity Identity { get; private set; }
+
+        /// <summary>
+        /// Additional state values for the authentication session.
+        /// </summary>
+        public AuthenticationProperties Properties { get; private set; }
+    }
+}
